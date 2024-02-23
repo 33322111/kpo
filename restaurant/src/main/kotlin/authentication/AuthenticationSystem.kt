6664648039b0b459317dc0bfa8e79235fc4bb3e9
Иван Евсеев Романович <73618383+33322111@users.noreply.github.com>
2024-02-23 20:13:38 +0300

    Fixed an admin panel

--- restaurant/src/main/kotlin/authentication/AuthenticationSystem.kt
+++ restaurant/src/main/kotlin/authentication/AuthenticationSystem.kt
@@ -1,21 +1,6 @@
 package authentication
 
 open class AuthenticationSystem(private val userRepository: UserRepository) {
-    fun registerUser(username: String, password: String, userType: UserType) {
-        val existingUser = userRepository.getUser(username)
-        if (existingUser != null) {
-            println("Пользователь с таким логином уже существует.")
-            return
-        }
-
-        if (passwordsMatch(password, repeatPassword())) {
-            userRepository.createUser(username, password, userType)
-            println("Пользователь успешно зарегистрирован.")
-        } else {
-            println("Пароли не совпадают. Повторите ввод.")
-            registerUser(username, password, userType)
-        }
-    }
 
     open fun authenticateUser(username: String, password: String, userType: UserType): Boolean {
         val user = userRepository.getUser(username)
--- restaurant/src/main/kotlin/menu/editor/ExtendedAuthenticationSystem.kt
+++ restaurant/src/main/kotlin/menu/editor/ExtendedAuthenticationSystem.kt
@@ -15,7 +15,6 @@ class ExtendedAuthenticationSystem(
     }
 
     private fun adminMenu() {
-        val menuManager = InMemoryMenuManager("menu.txt")
 
         while (true) {
             println("Меню администратора:")
